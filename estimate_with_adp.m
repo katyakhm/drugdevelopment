41d3f5728e6d7dc8b2b4958c9afbba3ee1c32f96
ekhmelni <ekhmelni@SAUD-CLW-03922.ead.ubc.ca>
2022-06-08 13:59:30 -0700

    feat: write left- and right-hand sides of Bellman equations using approximate value functions given weights for stages 2,3 and 4.

--- estimate_with_adp.m
+++ estimate_with_adp.m
@@ -1,5 +1,5 @@
 clear;
-
+addpath 'C:\Users\ekhmelni\Dropbox\Seeds and Drugs\Pharma Projects\KatyaJMP\DrugAttrition_Model\Estimation'
 %%%%%%%%%%%%%%%%%%%%%%%DESCRIPTION%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %This code implements the value funciton approximation approach for ONE
 %disease using the TRUE transition probabilities
@@ -30,15 +30,34 @@ params=table2struct(params_use,'ToScalar',true);
 filename=fullfile(folder,"data_science_generated_4_noop_2_params.csv");
 science_data=readtable(filename);
 
+%Some supplemental arrays
+state_transition_names={'from0to1','from0to2','from0to3','from0to4','from1to2','from1to0','from2to3','from2to0','from3to4','from3to0','from4to0'};
+stage_from={0,0,0,0,1,1,2,2,3,3,4}';
+stage_to={1,2,3,4,2,0,3,0,4,0,0}';
+ctp_stage_name={'for1','for2','for3','for4'};
+
+
 %Number of diseases:
 n_indications=size(params.indications_num,1);
-%I will run the value function approximation for Depression - indication 9%
-indication_num_use=9;
+%I will run the value function approximation for Epilepsy - indication 10%
+indication_num_use=10;
 %Max firms for each stage:
 max_firms_all=[params.max_in_1,params.max_in_2,params.max_in_3,params.max_in_4];
 max_firms=max_firms_all(params.indications_num==indication_num_use,:);
 %Market size
 marketsize = params.marketsize(params.indications_num==indication_num_use,:);
+
+%Creating the state space
+%Matlab first iterates the 1-st element in the vector, then second, etc.
+%But Python does it the other way round. So I need to re-order.
+s_1=0:max_firms(1);
+s_2=0:max_firms(2);
+s_3=0:max_firms(3);
+s_4=0:max_firms(4);
+supportS=transpose(combvec(s_4,s_3,s_2,s_1));
+supportS=[supportS(:,4) supportS(:,3) supportS(:,2) supportS(:,1)];
+clear s_1 s_2 s_3 s_4;
+
 
 %%%Known parameters (estimated at step 1)%%%
 
@@ -47,6 +66,9 @@ lambdas.lambda_1=1/params.stageduration_1_years(params.indications_num==indicati
 lambdas.lambda_2=1/params.stageduration_2_years(params.indications_num==indication_num_use,:);
 lambdas.lambda_3=1/params.stageduration_3_years(params.indications_num==indication_num_use,:);
 lambdas.lambda_4=1/params.stageduration_4_years(params.indications_num==indication_num_use,:);
+%Generic entry:
+lambdas.lambda_4_e=1/params.years_till_generic(params.indications_num==indication_num_use,:);
+
 
 %Entry rates:
 entry_lambdas.lambda_1 = 1/params.entryduration_1_years(params.indications_num==indication_num_use,:);
@@ -63,6 +85,13 @@ years_till_generic = params.years_till_generic(params.indications_num==indicatio
 %Probability of the FDA approval (e.g., transitioning from stage 3 to stage 4)
 p_3 = params.p_3(params.indications_num==indication_num_use,:);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%Aggregating the known parameters%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+params_known.p_3=p_3;
+params_known.rho=rho;
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%Here are the structural parameters%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -74,12 +103,45 @@ theta_r = params.theta_r(params.indications_num==indication_num_use,:);
 %The competition effect:
 theta_s = params.theta_s(params.indications_num==indication_num_use,:);
 
+%Stage 3 flow costs:
+flow_cost_3 = params.c_3(params.indications_num==indication_num_use,:);
+%Stage 2 flow costs:
+flow_cost_2 = params.c_2(params.indications_num==indication_num_use,:);
 
+%Probability of receiving a good signal after stage 1:
+p_1=params.p_1(params.indications_num==indication_num_use,:);
+%Probability of receiving a good signal after stage 2:
+p_2=params.p_2(params.indications_num==indication_num_use,:);
 
 %Upload the data that has the states that appeared along the simulated path%
 folder = "C:\Users\ekhmelni\Dropbox\Seeds and Drugs\Pharma Projects\KatyaJMP\DrugAttrition_Model\Estimation\resultsother";
-filename=fullfile(folder,"data_main_generated_4_noop_2_params.csv");
+filename=fullfile(folder,"data_main_generated_4_noop_2_params_tmp.csv");
 data=readtable(filename);
+
+%Separating across decision stages (decisions are made only after stages 1
+%and 2)
+inds.for1=data.state_index((data.action==11 | data.action==10) & data.indication_num==indication_num_use);
+inds.for2=data.state_index((data.action==21 | data.action==20) & data.indication_num==indication_num_use);
+actions.for1=data.action((data.action==11 | data.action==10) & data.indication_num==indication_num_use)-10;
+actions.for2=data.action((data.action==21 | data.action==20) & data.indication_num==indication_num_use)-20;
+%Minus 10 and minus 20 just translates it into ones and zeroes
+
+%FIRST ATTEMPT - WITH THE THEORETICAL CTPS
+
+    %Upload the TRUE THEORETICAL CCPs 
+    folder = "C:\Users\ekhmelni\Dropbox\Seeds and Drugs\Pharma Projects\KatyaJMP\DrugAttrition_Model\Estimation\matfiles";
+    filename=fullfile(folder,"ccps_orig_allinds_3_tmp");
+    ccps = load(filename);
+    ccps=ccps.ccps_allinds;
+    ccps=ccps.("ind_"+string(indication_num_use));
+    
+    %Get the corresponding CTPs
+    %THESE ARE CCPs FROM
+    ctps = ccps;
+    ctps(:,1)=params.p_1(params.indications_num==indication_num_use,:)*ccps(:,1);
+    ctps(:,2)=params.p_2(params.indications_num==indication_num_use,:)*ccps(:,2);
+
+
 
 %Parameters for the VF approximation:
 %The number of basis functions:
@@ -88,62 +150,81 @@ n_basis = 200;
 %parameters are drawn:
 var_basis = 0.01;
 
-%Some supplemental arrays
-state_transition_names={'from0to1','from0to2','from0to3','from0to4','from1to2','from1to0','from2to3','from2to0','from3to4','from3to0','from4to0'};
-stage_from={0,0,0,0,1,1,2,2,3,3,4}';
-stage_to={1,2,3,4,2,0,3,0,4,0,0}';
-ctp_stage_name={'for1','for2','for3','for4'};
+
 
 %Getting the state data for Depression:
 data_state=[data.n_in_1, data.n_in_2, data.n_in_3, data.n_in_4];
 %Here for the states_to_use I will just drop the states where there is zero
 %firms in the corresponding stage
-states_to_use.for1=data_state(data_state(:,1)>0 & data.indication_num==indication_num_use,:);
-states_to_use.for2=data_state(data_state(:,2)>0 & data.indication_num==indication_num_use,:);
-states_to_use.for3=data_state(data_state(:,3)>0 & data.indication_num==indication_num_use,:);
-states_to_use.for4=data_state(data_state(:,4)>0 & data.indication_num==indication_num_use,:);
+states_to_use.for1=data_state((data.action==11 | data.action==10) & data.indication_num==indication_num_use,:);
+states_to_use.for2=data_state((data.action==21 | data.action==20) & data.indication_num==indication_num_use,:);
 
-%%%1.Creating the adjacent states associated with my own trantision
-%Notice that by construction of max_firms, one transition ahead
-%should not put me at the boundary
-states_adjacent_i = get_states_adjacent_i_adp(states_to_use,ctp_stage_name);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%Creating the adjacent states%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%%2. Creating states associated with competitor's transitions
-states_adjacent_j = get_states_adjacent_j_apd(states_to_use,max_firms,ctp_stage_name,state_transition_names,stage_from,stage_to);
+%%%1.Creating the adjacent states associated with my own trantision (for
+%%%transitions from 1 to 2 and transitions from 2 to 3)
+states_once_adjacent_i = get_states_once_adjacent_i_adp(states_to_use,max_firms,ctp_stage_name);
 
-%%%%%%%%%%%%
-%%%Checks%%%
-%%%%%%%%%%%%
+%%%2. Creating the adjacent-adjacent states associated with competitor's transitions (that will
+%%%follow my own transition from 1 to 2 or from 2 to 3)
+states_twice_adjacent_j = get_states_twice_adjacent_j_adp(states_once_adjacent_i,max_firms,state_transition_names,stage_from,stage_to);
 
-%Total rate of transitioning out of state (taking into account that at the
-%max some transitions cannot happen)
-not_at_max.for1=states_to_use.for1<max_firms;
-not_at_max.for2=states_to_use.for2<max_firms;
-not_at_max.for3=states_to_use.for3<max_firms;
-not_at_max.for4=states_to_use.for4<max_firms;
+%%%3. Creating adjacent-adjacent states associated with my own further
+%%%transition from stage 2 to stage 3 (from states_once_adjacent_i.for1)
+%%%or from stage 3 to stage 4 (from states_once_adjacent_i.for1)
+states_twice_adjacent_i = get_states_twice_adjacent_i_adp(states_once_adjacent_i,max_firms,ctp_stage_name);
 
+%%%4. Creating adjacent-adjacent-adjacent state associated with competitor's transitions (that will
+%%%follow my own transition from 3 to 4)
+states_thrice_adjacent_j = get_states_thrice_adjacent_j_adp(states_twice_adjacent_i,max_firms,state_transition_names,stage_from,stage_to);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%Combining the rates (the full discount factor)%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%Creating the associated transition probabilities%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-rates_vector.for4=entry_lambdas.lambda_1*not_at_max.for4(:,1)+entry_lambdas.lambda_2*not_at_max.for4(:,2)+entry_lambdas.lambda_3*not_at_max.for4(:,3)+entry_lambdas.lambda_4*not_at_max.for4(:,4)+...
-            +lambdas.lambda_1*states_to_use.for4(:,1)+lambdas.lambda_2*states_to_use.for4(:,2)+lambdas.lambda_3*states_to_use.for4(:,3)+lambdas.lambda_4*states_to_use.for4(:,4);
+%%%1.Creating the transition probabilities for competitor's transitions (that will
+%%%follow my own transition from 1 to 2 or from 2 to 3)
+ctps_once_adjacent_i = get_ctps_once_adjacent_i_adp_theory(states_once_adjacent_i,supportS,ctps,ctp_stage_name);
 
-discount_rates.for4 = rates_vector.for4 + rho + 1/years_till_generic;
-
-rates_vector.for3=entry_lambdas.lambda_1*not_at_max.for3(:,1)+entry_lambdas.lambda_2*not_at_max.for3(:,2)+entry_lambdas.lambda_3*not_at_max.for3(:,3)+entry_lambdas.lambda_4*not_at_max.for3(:,4)+...
-            +lambdas.lambda_1*states_to_use.for3(:,1)+lambdas.lambda_2*states_to_use.for3(:,2)+lambdas.lambda_3*states_to_use.for3(:,3)+lambdas.lambda_4*states_to_use.for3(:,4);
-
-discount_rates.for3 = rates_vector.for3 + rho;
-
-rates_vector.for2=entry_lambdas.lambda_1*not_at_max.for2(:,1)+entry_lambdas.lambda_2*not_at_max.for2(:,2)+entry_lambdas.lambda_3*not_at_max.for2(:,3)+entry_lambdas.lambda_4*not_at_max.for2(:,4)+...
-            +lambdas.lambda_1*states_to_use.for2(:,1)+lambdas.lambda_2*states_to_use.for2(:,2)+lambdas.lambda_3*states_to_use.for2(:,3)+lambdas.lambda_4*states_to_use.for2(:,4);
-
-discount_rates.for2 = rates_vector.for2 + rho;
+%%%1.Creating the transition probabilities for competitor's transitions (that will
+%%%follow my own transition from 3 to 4)
+ctps_twice_adjacent_i = get_ctps_twice_adjacent_i_adp_theory(states_twice_adjacent_i,supportS,ctps);
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%Creating the coefficients for the right-hand side of the Bellmans%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%Here I create coefficients in front of the value functions associated 
+% with potential competitors' entries/transitions/terminations for the
+%right-hand side of the Bellman equations
+%Coefficients include:
+    %1. Rates lambda - estimated in the first step
+    %2. CTPs - estimated in the first step
+    %3. State vector - known
+%I also create the rates vector that is used to discount the right-hand
+%side of the Bellman equation (those are stored in the .sum part of the 
+% appropriate structure)
+
+%1. Coefficients following firm i's own transition from 1 to 2 or from 2 to 3
+%get_coefficients_base_adp creates coefficients in the same order as%%%
+%state_transition_names={'from0to1','from0to2','from0to3','from0to4','from1to2','from1to0','from2to3','from2to0','from3to4','from3to0','from4to0'};
+for origin_stage=1:2
+    current_stage=origin_stage+1;
+    states=states_once_adjacent_i.(ctp_stage_name{origin_stage}).states;
+    ctps=ctps_once_adjacent_i.(ctp_stage_name{origin_stage});
+    coefficients_once_adjacent_i.(ctp_stage_name{origin_stage}) = get_coefficients_base_adp(states,current_stage,ctps,params_known,entry_lambdas,lambdas,max_firms,state_transition_names);
+    %Add the discount factor:
+    coefficients_once_adjacent_i.(ctp_stage_name{origin_stage}).sum = coefficients_once_adjacent_i.(ctp_stage_name{origin_stage}).sum + params_known.rho;
+end
+
+%2. Coefficients following firm i's own transition from 3 to 4
+coefficients_twice_adjacent_i.for2 = get_coefficients_base_adp(states_twice_adjacent_i.for2.states,4,ctps_twice_adjacent_i.for2,params_known,entry_lambdas,lambdas,max_firms,state_transition_names);
+%Add the discount factor (augmented due to the probability of generic entry
+%as effectively they are equivalent):
+coefficients_twice_adjacent_i.for2.sum=coefficients_twice_adjacent_i.for2.sum + lambdas.lambda_4_e + params_known.rho;
 
 
 %%%%%%%%%%%%%%%%%%%%
@@ -165,45 +246,138 @@ discount_rates.for2 = rates_vector.for2 + rho;
 
 %1. Create the the 4-th order polynomial associated with the state
 %variables s_1, s_2, s_3, s_4 for the LEFT-HAND side
-polyvars_4_lh=create_polyvars(states_to_use.for4).vars;
+%The order in which the resulting variables are stored is in
+%create_polyvars(states_twice_adjacent_i.for2.states).varnames;
+states_twice_adjacent_i_for2_polyvars=create_polyvars(states_twice_adjacent_i.for2.states).vars;
+polynomial_varorder=create_polyvars(states_twice_adjacent_i.for2.states).varnames;
 
 %1. Create the the 4-th order polynomials associated with the state
 %variables s_1, s_2, s_3, s_4 for the RIGHT-HAND side
-
 for j = 1:numel(state_transition_names)
-    state=states_adjacent_j.for4.(state_transition_names{j});
-    states_adjacent_j_to_polyvars.for4.(state_transition_names{j}) = create_polyvars(state).vars;
+    state=states_thrice_adjacent_j.(state_transition_names{j}).states;
+    states_thrice_adjacent_j_polyvars.(state_transition_names{j}) = create_polyvars(state).vars;
 end
-
-
 
 
 %%%%%%WEIGHTS HERE WILL CHANGE%%%%%%%%%%%%%%%%%%%%
 
 %Initial guess for the weights for V_4:
-weights_4=normrnd(0,1,[size(polyvars_4_lh,2),1]);
+weights_4=normrnd(0,1,[size(states_twice_adjacent_i_for2_polyvars,2),1]);
 
 %The LEFT-HAND side of the Bellman, approximated given polynomial order
 %and the weights:
-V_4_lh=polyvars_4_lh*weights_4;
+V_4_lh=states_twice_adjacent_i_for2_polyvars*weights_4;
 
 %The V-s from the RIGHT-HAND side of the Bellman, approximated given polynomial order
 %and the weights:
+V_4_rh_j_separate=zeros(size(states_twice_adjacent_i_for2_polyvars,1),numel(state_transition_names));
 for j = 1:numel(state_transition_names)
-    V_4_rh.(state_transition_names{j})=states_adjacent_j_to_polyvars.for4.(state_transition_names{j})*weights_4;
+    V_4_rh_j_separate(:,j)=states_thrice_adjacent_j_polyvars.(state_transition_names{j})*weights_4;
 end
+
+%The full RIGHT-HAND side of the Bellman:
+
+V_4_rh_j_total=sum( V_4_rh_j_separate.*coefficients_twice_adjacent_i.for2.separate,2);
+r_4=profits_5(states_twice_adjacent_i.for2.states(:,4),marketsize,theta_0,theta_r,theta_s);
+%While in stage 4, the firm receives the flow profit, and at some rate it
+%has to exit and get the scrape walue, which is eulergamma in expectation 
+V_4_rh_own = r_4 + lambdas.lambda_4.*double(eulergamma);
+V_4_rh=(1./(coefficients_twice_adjacent_i.for2.sum)).*(V_4_rh_own+V_4_rh_j_total);
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
+%%%%%%%%%%%
+%%% V_3 %%%
+%%%%%%%%%%%
+
+%1. Create the the 4-th order polynomial for the LEFT-HAND side
+states_once_adjacent_i_for2_polyvars=create_polyvars(states_once_adjacent_i.for2.states).vars;
+
+%2. Create the the 4-th order polynomials associated with the state
+%variables s_1, s_2, s_3, s_4 for the RIGHT-HAND side
+for j = 1:numel(state_transition_names)
+    state=states_twice_adjacent_j.for2.(state_transition_names{j}).states;
+    states_twice_adjacent_j_for2_polyvars.(state_transition_names{j}) = create_polyvars(state).vars;
+end
+
+%%%%%%WEIGHTS HERE WILL CHANGE%%%%%%%%%%%%%%%%%%%%
+
+%Initial guess for the weights for V_3:
+weights_3=normrnd(0,1,[size(states_once_adjacent_i_for2_polyvars,2),1]);
+
+%The LEFT-HAND side of the Bellman, approximated given polynomial order
+%and the weights:
+V_3_lh=states_once_adjacent_i_for2_polyvars*weights_3;
+
+%The V-s from the RIGHT-HAND side of the Bellman, approximated given polynomial order
+%and the weights:
+V_3_rh_j_separate=zeros(size(states_once_adjacent_i_for2_polyvars,1),numel(state_transition_names));
+for j = 1:numel(state_transition_names)
+    V_3_rh_j_separate(:,j)=states_twice_adjacent_j_for2_polyvars.(state_transition_names{j})*weights_3;
+end
+
+V_3_rh_j_total=sum( V_3_rh_j_separate.*coefficients_once_adjacent_i.for2.separate,2);
+%While in stage 3, the firm pays the flow cost, and at some rate it
+%has to either transition to the next stage, or to exit and get the scrape walue, which is eulergamma in expectation 
+%Notice that if the next stage is full, it has to exit (I increase the
+%limits on the number of firms in each stage to ensure that that never
+%happens)
+V_3_rh_own=flow_cost_3+(lambdas.lambda_3).*(params_known.p_3).*(states_twice_adjacent_i.for2.not_at_constraint).*V_4_lh+(lambdas.lambda_3).*(1-(params_known.p_3).*(states_twice_adjacent_i.for2.not_at_constraint)).*double(eulergamma);
+V_3_rh=(1./(coefficients_once_adjacent_i.for2.sum)).*(V_3_rh_own+V_3_rh_j_total);
 
 
 
 
+%%%%%%%%%%%
+%%% V_2 %%%
+%%%%%%%%%%%
+
+%1. Create the the 4-th order polynomial for the LEFT-HAND side
+states_once_adjacent_i_for1_polyvars=create_polyvars(states_once_adjacent_i.for1.states).vars;
+
+%2. Create the the 4-th order polynomials associated with the state
+%variables s_1, s_2, s_3, s_4 for the RIGHT-HAND side
+for j = 1:numel(state_transition_names)
+    state=states_twice_adjacent_j.for1.(state_transition_names{j}).states;
+    states_twice_adjacent_j_for1_polyvars.(state_transition_names{j}) = create_polyvars(state).vars;
+end
+
+%3. Create the the 4-th order polynomials associated with the transition to
+%the next stage, stage 3 (so for V_3) on the RIGHT-HAND side
+
+states_twice_adjacent_i_for1_polyvars=create_polyvars(states_twice_adjacent_i.for1.states).vars;
+
+%%%%%%WEIGHTS HERE WILL CHANGE%%%%%%%%%%%%%%%%%%%%
+
+%Initial guess for the weights for V_3:
+weights_2=normrnd(0,1,[size(states_once_adjacent_i_for1_polyvars,2),1]);
+
+%The LEFT-HAND side of the Bellman, approximated given polynomial order
+%and the weights:
+V_2_lh=states_once_adjacent_i_for1_polyvars*weights_2;
+
+%The V-s from the RIGHT-HAND side of the Bellman, approximated given polynomial order
+%and the weights:
+V_2_rh_j_separate=zeros(size(states_once_adjacent_i_for1_polyvars,1),numel(state_transition_names));
+for j = 1:numel(state_transition_names)
+    V_2_rh_j_separate(:,j)=states_twice_adjacent_j_for1_polyvars.(state_transition_names{j})*weights_2;
+end
+
+V_2_rh_j_total=sum( V_2_rh_j_separate.*coefficients_once_adjacent_i.for1.separate,2);
+%While in stage 2, the firm pays the flow cost, and at some rate it
+%has to either transition to the next stage, or to exit and get the scrape walue, which is eulergamma in expectation 
+%Notice that if the next stage is full, it has to exit (I increase the
+%limits on the number of firms in each stage to ensure that that never
+%happens)
+V_3_rh_i_stage2=states_twice_adjacent_i_for1_polyvars*weights_3;
+expect_max_2=log(1+exp(V_3_rh_i_stage2))+double(eulergamma);
+V_2_rh_own=flow_cost_2+(lambdas.lambda_2).*p_2.*(states_twice_adjacent_i.for1.not_at_constraint).*expect_max_2+(lambdas.lambda_2).*(1-p_2.*(states_twice_adjacent_i.for1.not_at_constraint)).*double(eulergamma);
+V_2_rh=(1./(coefficients_once_adjacent_i.for1.sum)).*(V_2_rh_own+V_2_rh_j_total);
 
 
-
-
+%%%Next step - write it as a function with respect to the weights%%%
 
 
 
